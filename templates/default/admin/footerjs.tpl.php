ebc30686e23c4289382a1b6ebc0233a13d64498a
Erin Jo Richey <richey.erin@gmail.com>
2014-12-09 16:10:39 -0800

    Updates to Custom JS

--- templates/default/admin/footerjs.tpl.php
+++ templates/default/admin/footerjs.tpl.php
@@ -1,40 +1,40 @@
 <div class="row">
 
     <div class="span10 offset1">
-        <h1>Footer Javascript</h1>
+        <h1>Custom JavaScript & HTML</h1>
         <?=$this->draw('admin/menu')?>
+        <div class="explanation">
+            <p>
+            This editor lets you easily customize your website using a combination of JavaScript and HTML in the header and footer. You can use this space to add code to your site pages for things like analytics, page optimization, and customer tracking.                    </p>
+        </div>
     </div>
 
 </div>
-<div class="row">
-    <div class="span10 offset1">
-        <form action="/admin/footerjs/" class="form-horizontal" method="post">
-            <div class="control-group">
-                <div class="controls">
-                    <p>
-                        To include analytics code, for example Google Analytics or Piwik, in your site pages,
-                        paste it below:
-                    </p>
+<form action="/admin/footerjs/" class="form-horizontal" method="post">
+	<div class="row">
+		<div class="span10 offset1">
+		<h2>Code editor</h2>
+		</div>
+	</div>
+            <div class="row">
+                <div class="span10 offset1">
+	                <p class="controls"><strong>Header code</strong></p>
+                    <textarea name="headerjs" class="span10" rows="10"><?=htmlspecialchars(\Idno\Core\site()->config()->headerjs)?></textarea>
                 </div>
             </div>
-            <div class="control-group">
-                <label class="control-label" for="footerjs">Header Code</label>
-                <div class="controls">
-                    <textarea name="headerjs" class="span6" rows="10"><?=htmlspecialchars(\Idno\Core\site()->config()->headerjs)?></textarea>
+            <div class="row">
+               <!-- <label class="control-label" for="footerjs"><strong>Footer Code</strong></label>-->
+                <div class="span10 offset1">
+	               <p class="controls"><strong>Footer code</strong></p> 
+                    <textarea name="footerjs" class="span10" rows="10"><?=htmlspecialchars(\Idno\Core\site()->config()->footerjs)?></textarea>
                 </div>
             </div>
-            <div class="control-group">
-                <label class="control-label" for="footerjs">Footer Code</label>
-                <div class="controls">
-                    <textarea name="footerjs" class="span6" rows="10"><?=htmlspecialchars(\Idno\Core\site()->config()->footerjs)?></textarea>
-                </div>
-            </div>
-            <div class="control-group">
-                <div class="controls">
-                    <button type="submit" class="btn-primary">Save</button>
+            <div class="row">
+                <div class="span10 offset1">
+	             
+                    <button type="submit" class="btn btn-primary code">Save code</button>
+	                
                 </div>
             </div>
             <?= \Idno\Core\site()->actions()->signForm('/admin/footerjs/')?>
         </form>
-    </div>
-</div>
